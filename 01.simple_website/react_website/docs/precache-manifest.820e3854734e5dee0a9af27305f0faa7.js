4d1a2f6df831f018bd9e48b558f69b2dfc9b3f8d
Anton Cherepov <chekitdnb@gmail.com>
2019-10-14 22:29:59 +0200

    rebuild react

---
+++ 01.simple_website/react_website/docs/precache-manifest.820e3854734e5dee0a9af27305f0faa7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ba7e8b635897d7921ff8",
+    "url": "/static/js/main.ba7e8b63.chunk.js"
+  },
+  {
+    "revision": "2e86a8e0f1781f10c02b",
+    "url": "/static/js/1.2e86a8e0.chunk.js"
+  },
+  {
+    "revision": "ba7e8b635897d7921ff8",
+    "url": "/static/css/main.768b87da.chunk.css"
+  },
+  {
+    "revision": "550a33bf1cfa14a65188c5a09232a177",
+    "url": "/index.html"
+  }
+];
